a099b31ff6fc43a589a25207636c8060594287c0
董招兵 <1060545231@qq.com>
2016-09-18 15:57:53 +0800

    初始化代码仓库

---
+++ BKBadgeValue/BKBadgeValue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BKBadgeValue
+//
+//  Created by 董招兵 on 2016/9/18.
+//  Copyright © 2016年 大兵布莱恩特. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
